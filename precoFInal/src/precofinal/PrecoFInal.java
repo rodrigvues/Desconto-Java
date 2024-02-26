99987cc474f6ce51f122c15457e01b1f3f5d7158
Vitor Rodrigues <129552443+rodrigvues@users.noreply.github.com>
2024-02-26 08:44:36 -0300

    primeiro commit

---
+++ precoFInal/src/precofinal/PrecoFInal.java
@@ -0,0 +1,44 @@
+package precofinal;
+import java.util.Scanner;
+
+// Escreva um programa em Java para calcular o preço final de 
+// um produto com desconto, considerando as seguintes regras:
+public class PrecoFInal {
+    public static void main(String[] args) {
+        double preco, precoFinal;
+        char fimProg = 'S';
+        Scanner leia = new Scanner(System.in);
+        
+        while(fimProg == 'S'){ 
+            // Solicite ao usuário que insira o valor original do produto. 
+            System.out.println("Informe o preço do produto : ");
+            preco = leia.nextDouble();
+
+            // Se o valor do produto for superior a R$ 100, aplique um 
+            // desconto de 15%. 
+            // Caso contrário, se o valor do produto estiver entre R$ 50 e R$ 100, aplique um desconto de 10%.
+            // Se o valor do produto for inferior a R$ 50, não aplique desconto. 
+
+            if (preco >= 100) {
+                precoFinal = preco - (preco * 0.15);
+            }
+            else if (preco >= 50 && preco < 100) {
+                precoFinal = preco - (preco * 0.1);
+            }
+            else {
+                precoFinal = preco;
+            }
+
+            // Exiba na tela o preço final do produto após o desconto.
+            System.out.println("Preço Final : "+precoFinal);
+
+            System.out.println("Deseja informar outro produto? (S/N) ");
+            fimProg = leia.next().toUpperCase().charAt(0);
+            while (fimProg != 'S' && fimProg != 'N') {
+            System.out.println("Opcao Invalida. (S/N)");
+            fimProg = leia.next().toUpperCase().charAt(0);} 
+            }
+        }
+}
+
+
